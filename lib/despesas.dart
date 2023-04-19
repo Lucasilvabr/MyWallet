10df70b16c67b6db5119eaa28fdcaa9ba55fb77d
da Silva <Lucas.daSilva1@br.nestle.com>
2023-04-19 16:19:09 -0300

    adicionando navigator em despesas

---
+++ lib/despesas.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Expense {
+  String name;
+  double value;
+
+  Expense(this.name, this.value);
+}
+
+class ExpenseListPage extends StatelessWidget {
+  final List<Expense> expenses = [
+    Expense('Comida', 50),
+    Expense('Transporte', 20),
+    Expense('Lazer', 100),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas'),
+      ),
+      body: ListView.builder(
+        itemCount: expenses.length,
+        itemBuilder: (BuildContext context, int index) {
+          Expense expense = expenses[index];
+          return ListTile(
+            title: Text(expense.name),
+            trailing: Text('R\$ ${expense.value.toStringAsFixed(2)}'),
+          );
+        },
+      ),
+    );
+  }
+}
