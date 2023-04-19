94fb22d50cc845316862795c458f4980b2e6af6f
da Silva <Lucas.daSilva1@br.nestle.com>
2023-04-19 13:50:05 -0300

    atualizacao

--- lib/menu.dart
+++ lib/menu.dart
@@ -2,6 +2,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/deposito.dart';
+import 'package:flutter_application_1/cartoes.dart';
 
 class menu extends StatelessWidget {
   @override
@@ -26,6 +27,26 @@ class menu extends StatelessWidget {
                   MaterialPageRoute(builder: (context) => deposito()),
                 );
               },
+              child: const Text('Depositar',
+                      style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 50,
+                          color: Colors.white)),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromRGBO(41, 182, 246, 100)),
+                    fixedSize: MaterialStateProperty.all<Size>(
+                        Size(buttonWidth, buttonHeight)),
+              ),
+            ),
+            SizedBox(height: 30),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => cartoes()),
+                );
+              },
               child: ElevatedButton(
                 onPressed: () {},
                 child: const Text(
@@ -41,23 +62,6 @@ class menu extends StatelessWidget {
                   fixedSize: MaterialStateProperty.all<Size>(
                       Size(buttonWidth, buttonHeight)),
                 ),
-              ),
-            ),
-            SizedBox(height: 30),
-            
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('  Cartões ',
-                      style: TextStyle(
-                          fontFamily: 'Poppins',
-                          fontSize: 50,
-                          color: Colors.white)),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                        const Color.fromRGBO(41, 182, 246, 100)),
-                    fixedSize: MaterialStateProperty.all<Size>(
-                        Size(buttonWidth, buttonHeight)),
-                        
               ),
             ),
             SizedBox(height: 30),
